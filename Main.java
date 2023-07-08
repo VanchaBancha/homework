f33f16e4bdae7dc1adab213d56a1f537b3f269ca
VanchaBancha <132605816+VanchaBancha@users.noreply.github.com>
2023-07-08 21:44:51 +0200

    Variables ( String and int )

---
+++ Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String...args){
+
+        String text1 = "Hi Martin!";
+        String text2 = "Hi Maks!";
+        String text3 = "Nice";
+
+        int number1 = 8;
+        int number2 = 16;
+        int number3 = 80;
+
+        System.out.println(text1);
+        System.out.println(text2);
+        System.out.println(text3);
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+    }
+}
