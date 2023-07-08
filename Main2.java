c1ba9c22724f5b5aa15127d3b05c581786a68e26
VanchaBancha <132605816+VanchaBancha@users.noreply.github.com>
2023-07-08 21:58:48 +0200

    Variables ( +, -, *, / )

---
+++ Main2.java
@@ -0,0 +1,19 @@
+public class Main2{
+    public static void main(String...args) {
+
+        int number1 = 8;
+        int number2 = 14;
+
+        int number3 = number1 + number2;
+        int number4 = number1 - number2;
+        int number5 = number1 * number2;
+        int number6 = number1 / number2;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+    }
+}
